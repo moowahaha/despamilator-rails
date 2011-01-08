cca16001c9d776a83263f86bed478b15ee2c97e3
Stephen Hardisty <moowahaha@hotmail.com>
2011-01-09 10:48:23 +1100

    removed needless use of "send"

--- lib/despamilator_rails.rb
+++ lib/despamilator_rails.rb
@@ -35,36 +35,34 @@ module ActiveRecord
           attribute.blank?
         end
 
-        clean_attributes.empty? ? raise('At least one attribute must be defined') : define_method('despamilator.attributes', lambda { clean_attributes })
+        clean_attributes.empty? ? raise('At least one attribute must be defined') : define_method('despamilator_checked_attributes', lambda { clean_attributes })
       end
 
       def assign_despamilator_threshold(settings)
-        define_method('despamilator.threshold', lambda { settings[:threshold] || 1 })
+        define_method('despamilator_threshold', lambda { settings[:threshold] || 1 })
       end
 
       def assign_despamilator_callback(block)
-        define_method('despamilator.callback', block || default_despamilator_detection_response)
+        define_method('despamilator_callback', block || default_despamilator_detection_response)
       end
 
+      def default_despamilator_detection_response
+        lambda { |attribute, value, dspam|
+          raise "#{attribute} exceeded the spam threshold of #{despamilator_threshold} (scored: #{dspam.score}, value: #{value})"
+        }
+      end
+      
     end
 
     def validate_with_despamilator_checks
       old_spamilated_validation
 
-      threshold = send('despamilator.threshold')
-
-      send('despamilator.attributes').each do |attribute|
+      despamilator_checked_attributes.each do |attribute|
         text  = self.send(attribute)
         dspam = Despamilator.new(text)
 
-        send('despamilator.callback', attribute, text, dspam) if dspam.score >= threshold
+        despamilator_callback(attribute, text, dspam) if dspam.score >= despamilator_threshold
       end
-    end
-
-    def self.default_despamilator_detection_response
-      lambda { |attribute, value, dspam|
-        raise "#{attribute} exceeded the spam threshold of #{send('despamilator.threshold')} (scored: #{dspam.score}, value: #{value})"
-      }
     end
 
   end

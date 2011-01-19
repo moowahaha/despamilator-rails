2f333aee573a1d027910ae93c2865054e2c656c6
Stephen Hardisty <moowahaha@hotmail.com>
2011-01-19 20:32:46 +1100

    added todo

--- spec/data_mapper_integration_spec.rb
+++ spec/data_mapper_integration_spec.rb
@@ -1,4 +1,5 @@
 describe "data mapper integration" do
+  # todo refactor to shared spec
   it "should check for spam on save" do
     some_instance            = SomeDataMapperModel.new
     some_instance.some_field = Despamilator.gtubs_test_string
